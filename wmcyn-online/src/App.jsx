fc01d2cf6bc17dc0e63888859bf254a51ee8468f
jvredmoore <jvredmoore@gmail.com>
2023-05-26 11:52:50 -0400

    finished front-end landing page

---
+++ wmcyn-online/src/App.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import Typewriter from 'typewriter-effect';
+import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faArrowDown, faArrowUp);
+
+import logo from './assets/WMCYN LOGO WHITE.png';
+
+const App = () => {
+  const [email, setEmail] = useState('');
+  const [isPlaceholderVisible, setPlaceholderVisible] = useState(true);
+  const [password, setPassword] = useState('');
+  const [isArrowUp, setArrowUp] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleEmailClick = () => {
+    if (isPlaceholderVisible) {
+      setPlaceholderVisible(false);
+    }
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle the form submission here
+    console.log('Email submitted:', email);
+    console.log('Password submitted:', password);
+  };
+
+  const scrollToFriendsAndFamily = () => {
+    const friendsAndFamilySection = document.getElementById('friendsAndFamilySection');
+    friendsAndFamilySection.scrollIntoView({ behavior: 'smooth' });
+    setArrowUp(true);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    setArrowUp(false);
+  };
+
+  return (
+    <div>
+      <div className="container">
+      <img src={logo} alt="Logo" className="logo" />
+        <h1 className="typewriter">
+          <Typewriter
+            options={{
+              strings: ["YOU'RE EARLY...", "SIGN UP FOR OUR NEWSLETTER"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder={isPlaceholderVisible ? 'Enter your email' : ''}
+            value={email}
+            onChange={handleEmailChange}
+            onClick={handleEmailClick}
+            className="input-field"
+          />
+          <button type="submit" className="submit-button">Subscribe</button>
+        </form>
+      </div>
+
+      <div id="friendsAndFamilySection" className="container friends-and-family-section">
+        <h2 className="section-heading">FRIENDS AND FAMILY SHOP</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={handlePasswordChange}
+            className="input-field"
+          />
+          <button type="submit" className="submit-button">Enter</button>
+        </form>
+      </div>
+
+      <div className="scroll-button-container">
+  <button
+    className={`scroll-button ${isArrowUp ? 'up' : 'down'}`}
+    onClick={isArrowUp ? scrollToTop : scrollToFriendsAndFamily}
+  >
+    <FontAwesomeIcon icon={isArrowUp ? 'arrow-up' : 'arrow-down'} />
+  </button>
+</div>
+    </div>
+  );
+};
+
+export default App;
