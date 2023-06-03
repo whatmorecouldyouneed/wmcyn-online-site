b88d45bd8ecea6ca79cdb59b2ef3f720a29c7fcc
jvredmoore <jvredmoore@gmail.com>
2023-06-02 21:39:50 -0400

    fixed vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    base: '/whatmorecouldyouneed.github.io/',
+    base: 'https://whatmorecouldyouneed.github.io/',
   },
 });
